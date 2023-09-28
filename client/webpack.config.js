955808fc68beba1e831dd41f60d21de1ed1c2704
Khan-HB <hafsah999@gmail.com>
2023-09-28 02:16:42 -0400

    added commments in starter code, finished the todos in webpack config file, may revisit it later.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,27 +3,81 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
+    // Set the build mode to development for now, change to 'production' for production builds.
     mode: 'development',
+    
+    // Define entry points for your application.
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: './src/js/index.js',     // Main application entry point
+      install: './src/js/install.js' // Entry point for installation-related code
     },
+    
+    // Configure the output directory and filename for bundled JavaScript files.
     output: {
       filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname, 'dist'), // Output directory
     },
+    
+    // Define plugins to enhance Webpack's functionality.
     plugins: [
+      // HtmlWebpackPlugin configuration to generate HTML files.
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Just Another Text Editor'
+      }),
       
+      // WebpackPwaManifest configuration to generate a manifest file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+            purpose: 'maskable',         
+          },
+        ],
+      }),
+      
+      // InjectManifest configuration for service worker integration.
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
     ],
 
+    // Define rules for processing different types of files (e.g., CSS, Babel for JavaScript).
     module: {
+      // CSS loaders
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
+// Import the 'express' library
 const express = require('express');
 
+// Create an instance of the Express application
 const app = express();
+
+// Define the port where the server will listen, using the value from the environment variable 'PORT' or default to 3000
 const PORT = process.env.PORT || 3000;
 
+// Middleware: Serve static files from the '../client/dist' directory
 app.use(express.static('../client/dist'));
+
+// Middleware: Parse incoming URL-encoded data and JSON data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Require and use the HTML routes defined in './routes/htmlRoutes.js'
 require('./routes/htmlRoutes')(app);
 
+// Start the server and listen on the specified port
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
